4ec1453c067b797a0cd42a26bb60bb927f59a4b0
Damon Oehlman <damon.oehlman@gmail.com>
2011-10-25 08:55:07 +1000

    Added a reporter callback to quieten down logging

--- dr.js
+++ dr.js
@@ -15,30 +15,30 @@ function getPath(filepath) {
     return "docs/" + path.basename(filepath, path.extname(filepath));
 }
 
-module.exports = function(files, targetDir) {
+module.exports = function(files, targetDir, reporter) {
     
     
     exec("mkdir -p " + path.resolve(targetDir, "docs"));
     exec("cp " + __dirname + "/dr.css " + path.resolve(targetDir, "docs/dr.css"));
 
     files.forEach(function (filename) {
-        console.log("\nTrust me, I am a Dr.js\n\nProcessing " + filename);
+        reporter || console.log("\nTrust me, I am a Dr.js\n\nProcessing " + filename);
         fs.readFile(filename, "utf-8", function(error, code) {
             if (error) {
                 throw error;
             }
             var res = docit(code, filename);
             if (res && res.sections && res.doc) {
-                console.log("Found \033[32m" + res.sections + "\033[0m sections.");
-                console.log("Processing \033[32m" + res.loc + "\033[0m lines of code...");
+                reporter || console.log("Found \033[32m" + res.sections + "\033[0m sections.");
+                reporter || console.log("Processing \033[32m" + res.loc + "\033[0m lines of code...");
 
                 fs.writeFile(path.resolve(targetDir, getPath(filename) + ".html"), res.doc, function () {
                     fs.writeFile(path.resolve(targetDir, getPath(filename) + "-src.html"), res.source, function () {
-                        console.log("Saved to \033[32m" + getPath(filename) + "\033[0m and \033[32m" + getPath(filename, 1) + "\033[0m\n");
+                        (reporter ? reporter : console.log)("Saved to \033[32m" + getPath(filename) + "\033[0m and \033[32m" + getPath(filename, 1) + "\033[0m\n");
                     });
                 });
             } else {
-                console.log("\033[31mNo comments in Dr.js format found\033[0m");
+                (reporter ? reporter : console.log)("\033[31mNo comments in Dr.js format found\033[0m");
             }
         });
     });
