9d23d6e57c2a920c6265a8b20b54ac58280b75d9
Dmitry Baranovskiy <dmitry@baranovskiy.com>
2013-11-15 15:00:37 +1100

    0.1.0 Added templating

--- doc.js
+++ doc.js
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 /*
- * Dr.js 0.0.10 - Simple JavaScript Documentation
+ * Dr.js 0.1.0 - Simple JavaScript Documentation
  *
  * Author: Dmitry Baranovskiy (http://dmitry.baranovskiy.com/)
  */
@@ -44,13 +44,6 @@ module.exports = function (txt, filename, sourceFileName) {
         rdoc = /\/\*\\[\s\S]*?\\\*\//g,
         rdoc2 = /^\/\*\\\n\s*\*\s+(.*)\n([\s\S]*)\\\*\/$/,
         rows = /^\s*(\S)(?:(?!\n)\s(.*))?$/,
-        // rcode = /`([^`]+)`/g,
-        rkeywords = /\b(abstract|boolean|break|byte|case|catch|char|class|const|continue|debugger|default|delete|do|double|else|enum|export|extends|false|final|finally|float|for|function|goto|if|implements|import|in|instanceof|int|interface|long|native|new|null|package|private|protected|public|return|short|static|super|switch|synchronized|this|throw|throws|transient|true|try|typeof|var|void|volatile|while|with|undefined)\b/g,
-        rstrings = /("[^"]*?(?:\\"[^"]*?)*"|'[^']*?(?:\\'[^']*?)*')/g,
-        roperators = /( \= | \- | \+ | % | \* | \&\& | \&amp;\&amp; | \& | \&amp; | \|\| | \| | \/ | == | === )/g,
-        rdigits = /(\b(0[xX][\da-fA-F]+)|((\.\d+|\b\d+(\.\d+)?)(?:e[-+]?\d+)?))\b/g,
-        rcomments = /(\/\/.*?(?:\n|$)|\/\*(?:.|\s)*?\*\/)$/g,
-        // rhref = /(https?:\/\/[^\s"]+[\d\w_\-\/])/g,
         rlink = /(^|\s)@([\w\.\_\$]*[\w\_\$])/g,
         ramp = /&(?!\w+;|#\d+;|#x[\da-f]+;)/gi,
         rantiwrap = /^<([^>]+)>(.*)<\/\1>$/,
@@ -65,6 +58,9 @@ module.exports = function (txt, filename, sourceFileName) {
         srcfilename = sourceFileName || (path.basename(filename, path.extname(filename)) + "-src.html"),
         clas = "",
         TOC = [],
+        sections = [],
+        current = sections,
+        prev = sections,
         utoc = {},
         chunks = {},
         out = {};
@@ -77,76 +73,58 @@ module.exports = function (txt, filename, sourceFileName) {
             .replace(ramp, '<em class="amp">&amp;</em>')
             .replace(rlink, '$1<a href="#$2" class="dr-link">$2</a>');
     }
-    function syntax(text) {
-        return text.replace(/</g, "&lt;")
-            .replace(ramp, "&amp;")
-            .replace(rkeywords, "<b>$1</b>")
-            .replace(rstrings, "<i>$1</i>")
-            .replace(roperators, '<span class="s">$1</span>')
-            .replace(rdigits, '<span class="d">$1</span>')
-            .replace(rcomments, '<span class="c">$1</span>') + "\n";
-    }
-    function syntaxSrc(text) {
-        var isend = text.match(/\*\//);
-        if (text.match(/\/\*/)) {
-            syntaxSrc.inc = true;
-        }
-        var out = text.replace(/</g, "&lt;")
-            .replace(/&(?!\w+;|#\d+;|#x[\da-f]+;)/gi, "&amp;")
-            .replace(rkeywords, "<b>$1</b>")
-            .replace(rstrings, "<i>$1</i>")
-            .replace(roperators, '<span class="s">$1</span>')
-            .replace(rdigits, '<span class="d">$1</span>')
-            .replace(/(\/\*(?:.(?!\*\/))+(?:\*\/)?)/g, '<span class="c">$1</span>')
-            .replace(rcomments, '<span class="c">$1</span>') + "\n";
-        if (syntaxSrc.inc) {
-            out = out.replace(/(^.*\*\/)/, '<span class="c">$1</span>');
-            if (!isend) {
-                out = '<span class="c">' + out + '</span>';
-            }
-        }
-        if (isend) {
-            syntaxSrc.inc = false;
-        }
-        return out;
-    }
 
     eve.on("doc.*.list", function (mod, text) {
-        this != "-" && (html += "</ol></div>\n");
+        if (this != "-") {
+            current = prev;
+        }
     });
     eve.on("doc.*.json", function (mod, text) {
-        this != "o" && (html += "</ol>\n");
+        if (this != "o") {
+            current = prev;
+        }
     });
     eve.on("doc.*.text", function (mod, text) {
-        this != "*" && (html += "</p>\n");
-    });
-    eve.on("doc.*.head", function (mod, text) {
-        // this != "*" && (html += "</h3>\n");
+        if (this != "*") {
+            current = prev;
+        }
     });
     eve.on("doc.*.code", function (mod, text) {
-        this != "|" && (html += "</code></pre></section>\n");
+        if (this != "|") {
+            current = prev;
+        }
     });
     eve.on("doc.s*.*", function (mod, text) {
-        mode != "text" && (html += "<p>");
+        if (mode != "text") {
+            prev = current;
+            var arr = [""];
+            current.push({text: arr});
+            current = arr;
+        }
         if (text) {
-            html += esc(text) + "\n";
+            current[current.length - 1] += esc(text) + "\n";
         } else {
-            html += "</p>\n<p>";
+            current.push("");
         }
         mode = "text";
     });
     eve.on("doc.s|.*", function (mod, text) {
-        mode != "code" && (html += '<section class="code"><pre class="javascript code"><code data-language="javascript" class="language-javascript">');
-        html += text.replace(/</g, "&lt;").replace(ramp, "&amp;") + "\n";
+        if (mode != "code") {
+            prev = current;
+            var arr = [];
+            current.push({code: arr});
+            current = arr;
+        }
+        current.push(text);
         mode = "code";
     });
     eve.on("doc.s#.*", function (mod, text) {
-        html += text + "\n";
+        current.push({html: text + "\n"});
         mode = "html";
     });
     eve.on("doc.s>.*", function (mod, text) {
         mode != "head" && (html += '<h3>');
-        html += esc(text) + "</h3>";
+        current.push({head: esc(text)});
         mode = "head";
     });
     eve.on("doc.s[.*", function (mod, text) {
@@ -157,12 +135,9 @@ module.exports = function (txt, filename, sourceFileName) {
         });
         itemData.type = esc(text).replace(/\s*\]\s*$/, "").split(/\s*,\s*/);
         clas = itemData.clas = "dr-" + itemData.type.join(" dr-");
-        html += '<div class="' + clas + '">';
-        type && (html += '<em class="dr-type dr-type-' + type + '">' + type + '</em>');
         mode = "";
     });
     eve.on("doc.end.*", function (mod, text) {
-        clas && (html += "</div>");
     });
     eve.on("doc.s=.*", function (mod, text) {
         var split = text.split(/(\s*[\(\)]\s*)/);
@@ -170,18 +145,20 @@ module.exports = function (txt, filename, sourceFileName) {
         split.shift();
         var types = split.shift().split(/\s*\|\s*/);
         split.shift();
-        html += '<p class="dr-returns"><strong class="dr-title">Returns:</strong> ';
-        for (var i = 0, ii = types.length; i < ii; i++) {
-            types[i] = '<em class="dr-type-' + types[i] + '">' + types[i] + '</em>';
-        }
-        html += types.join(" ") + ' <span class="dr-description">' + esc(split.join("")) + "</span></p>\n";
+        current.push({rtrn: {
+            type: types,
+            desc: esc(split.join(""))
+        }});
         mode = "";
     });
     eve.on("doc.s-.*", function (mod, text) {
         itemData.params = itemData.params || [];
         if (mode != "list") {
-            html += '<div class="topcoat-list__container"><h3 class="topcoat-list__header">Parameters</h3><ol class="topcoat-list">';
             itemData.params.push([]);
+            prev = current;
+            var arr = [];
+            current.push({attr: arr});
+            current = arr;
         }
         var optional,
             data = itemData.params[itemData.params.length - 1];
@@ -189,47 +166,52 @@ module.exports = function (txt, filename, sourceFileName) {
             optional = true;
             return "";
         });
-        var split = text.split(/(\s*[\(\)]\s*)/);
+        var split = text.split(/(\s*[\(\)]\s*)/),
+            item = {};
         data.push((optional ? "[" : "") + split[0] + (optional ? "]" : ""));
-        html += '<li class="topcoat-list__item"><span class="dr-param' + (optional ? " optional" : "") + '">' + split.shift() + '</span>\n';
+        item.name = split.shift();
         split.shift();
         if (optional) {
-            html += '<span class="dr-optional">optional</span>\n';
+            item.optional = true;
         }
         var types = split.shift().split(/\s*\|\s*/);
         split.shift();
-        html += '<span class="dr-type">';
-        for (var i = 0, ii = types.length; i < ii; i++) {
-            types[i] = '<em class="dr-type-' + types[i] + '">' + types[i] + '</em>';
-        }
-        html += types.join(" ") + '</span>\n<span class="dr-description">' + (esc(split.join("")) || "&#160;") + '</span></li>\n';
+        item.type = types;
+        var stypes = [];
+        item.desc = esc(split.join(""));
+        current.push(item);
         mode = "list";
     });
     eve.on("doc.so.*", function (mod, text) {
         if (mode != "json") {
-            html += '<ol class="dr-json">';
+            prev = current;
+            var arr = [];
+            current.push({json: arr});
+            current = arr;
         }
         var desc = text.match(/^\s*([^\(\s]+)\s*\(([^\)]+)\)\s*(.*?)\s*$/),
             start = text.match(/\s*\{\s*$/),
-            end = text.match(/\s*\}\s*,?\s*$/);
-        !end && (html += "<li>");
+            end = text.match(/\s*\}\s*,?\s*$/),
+            item = {};
         if (desc) {
-            html += '<span class="dr-json-key">' + desc[1] + '</span>';
+            item.key = desc[1];
+            item.type = desc[2].split(/\s*\|\s*/);
+            item.desc = esc(desc[3]);
             var types = desc[2].split(/\s*\|\s*/);
-            html += '<span class="dr-type">';
-            for (var i = 0, ii = types.length; i < ii; i++) {
-                types[i] = '<em class="dr-type-' + types[i] + '">' + types[i] + '</em>';
-            }
-            html += types.join(" ") + '</span><span class="dr-json-description">' + (esc(desc[3]) || "&#160;") + '</span>\n';
-        } else {
-            !end && (html += text);
+        } else if (!end) {
+            item = text;
         }
         if (start) {
-            html += '<ol class="dr-json">';
+            item = {
+                start: text
+            };
         }
         if (end) {
-            html += '</ol></li><li>' + text + '</li>';
+            item = {
+                end: text
+            };
         }
+        current.push(item);
         mode = "json";
     });
 
@@ -249,7 +231,6 @@ module.exports = function (txt, filename, sourceFileName) {
     for (var i = 0, ii = main.length; i < ii; i++) {
         var doc = main[i];
         line++;
-        src += '<code id="L' + line + '"><span class="ln">' + line + '</span>' + syntaxSrc(doc) + '</code>';
         if (doc.match(beginrg)) {
             inside = firstline = true;
             pointer = root;
@@ -258,7 +239,7 @@ module.exports = function (txt, filename, sourceFileName) {
             mode = "";
             continue;
         }
-        if (doc.match(endrg)) {
+        if (doc.match(endrg) && inside) {
             inside = false;
             eve("doc.end." + mode, null, mode, "");
             itemData.line = line + 1;
@@ -285,6 +266,7 @@ module.exports = function (txt, filename, sourceFileName) {
                     for (var j = 0, jj = title.length; j < jj; j++) {
                         pointer = pointer[title[j]] = pointer[title[j]] || {};
                     }
+                    sections.push(current = prev = [itemData]);
                 } else {
                     eve("doc.s" + symbol + "." + mode, symbol, mode, text);
                 }
@@ -297,7 +279,7 @@ module.exports = function (txt, filename, sourceFileName) {
         itemData,
         res = "";
     var runner = function (pointer, hx) {
-        var level = [], name, chunk;
+        var level = [], name, chunk, brackets;
         for (var node in pointer) {
             level.push(node);
         }
@@ -305,44 +287,27 @@ module.exports = function (txt, filename, sourceFileName) {
         for (var j = 0, jj = level.length; j < jj; j++) {
             lvl.push(level[j]);
             name = lvl.join(".");
-            html = "";
-            chunk = "";
             itemData = {};
             eve("doc.item", pointer[level[j]]);
-            chunk += '<article id="' + name + '" class="' + name.replace(/\./g, "-") + '-section"><header><h'
-                + hx + ' id="' + name + '" class="' + itemData.clas
-                + '">' + name;
+            itemData.title = name;
             if (itemData.type && itemData.type.indexOf("method") + 1) {
                 if (itemData.params) {
                     if (itemData.params.length == 1) {
-                        chunk += "(" + itemData.params[0].join(", ") + ")";
+                        brackets = "(" + itemData.params[0].join(", ") + ")";
                     } else if (!itemData.params.length) {
-                        chunk += "()";
+                        brackets = "()";
                     } else {
-                        chunk += "(\u2026)";
+                        brackets = "(\u2026)";
                     }
                 } else {
-                    chunk += "()";
+                    brackets = "()";
                 }
             }
-            chunk += '<a href="#' + name + '" title="Link to this section" class="dr-hash">&#x2693;</a>';
-            itemData.line && (chunk += '<a class="dr-sourceline" title="Go to line ' + itemData.line + ' in the source" href="' + srcfilename + '#L' + itemData.line + '">&#x27ad;</a>');
-            chunk += '</h' + hx + '></header>\n<section>';
-            chunk += '<div class="extra" id="' + name + '-extra"></div>';
-            chunk += html;
-            chunk += '</section></article>';
-            chunks[name] = chunks[name] || "";
-            chunks[name] += chunk;
-            res += chunk;
+            itemData.name = name + (brackets || "");
             var indent = 0;
             name.replace(/\./g, function () {
                 indent++;
             });
-            toc += '<li class="dr-lvl' + indent + '"><a href="#' + name + '" class="' + itemData.clas + '"><span>' + name;
-            if (itemData.type && itemData.type.indexOf("method") + 1) {
-                toc += "()";
-            }
-            toc += '</span></a></li>';
             if (!utoc[name]) {
                 TOC.push({
                     indent: indent,
@@ -358,10 +323,10 @@ module.exports = function (txt, filename, sourceFileName) {
     };
 
     runner(root, 2);
-    out.chunks = chunks;
     out.toc = TOC;
+    out.out = sections;
+    // console.log(JSON.stringify(sections));
     out.title = Title ? Title[1] : "";
-    out.source = '<!DOCTYPE html>\n<!-- Generated with Dr.js -->\n<html lang="en"><head><meta charset="utf-8"><title>' + path.basename(filename) + '</title><link rel="stylesheet" href="dr.css"></head><body id="src-dr-js">' + src + '</body></html>';
     eve.unbind("doc.*");
     return out;
 };
--- dr.js
+++ dr.js
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 /*
- * Dr.js 0.0.10 - Simple JavaScript Documentation
+ * Dr.js 0.1.0 - Simple JavaScript Documentation
  *
  * Author: Dmitry Baranovskiy (http://dmitry.baranovskiy.com/)
  */
@@ -20,6 +20,7 @@ var fs = require("fs"),
     path = require("path"),
     docit = require("./doc.js"),
     format = require("./formatter.js"),
+    dot = require("dot"),
     _ref = require("child_process"),
     // spawn = _ref.spawn,
     exec = _ref.exec;
@@ -102,7 +103,31 @@ function ss(num) {
  |   o     }
  |   o }
  |  \&#x2a;/
-
+\*/
+/*\
+ * Snap
+ [ method ]
+ **
+ * Creates a drawing surface or wraps existing SVG element.
+ **
+ - width (number|string) width of surface
+ - height (number|string) height of surface
+ * or
+ - DOM (SVGElement) element to be wrapped into Snap structure
+ * or
+ - query (string) CSS query selector
+ # And here comes <button type="button">HTML</button>
+ = (object) @Element
+ o {
+ o     min: {
+ o         x: (number) x coordinate of the left point
+ o         y: (number) y coordinate of the top point
+ o     }
+ o     max: {
+ o         x: (number) x coordinate of the right point
+ o         y: (number) y coordinate of the bottom point
+ o     }
+ o }
 \*/
 
 var files = process.argv.slice(0),
@@ -113,87 +138,92 @@ var files = process.argv.slice(0),
     output = "",
     scripts = [],
     fileName,
+    sec = [],
     toc = [];
 files.splice(0, 2);
 ref = files[0];
 if (!files.length) {
     console.log("\nUsage: node dr <conf.json>");
+} else {
+    if (files.length == 1 && path.extname(ref) == ".json") {
+        var pth = ref.substring(0, ref.length - path.basename(ref).length),
+            json = JSON.parse(fs.readFileSync(files[0], "utf-8"));
+        title = json.title;
+        files = [];
+        for (var i = 0, ii = json.files && json.files.length; i < ii; i++) {
+            files.push(path.resolve(pth, json.files[i].url));
+            srcs.push(json.files[i].link);
+        }
+        json.template = json.template && path.resolve(pth, json.template);
+        output = path.resolve(pth, json.output) || "";
+        pth = output.substring(0, output.length - path.basename(output).length);
+        if (json.css != "custom") {
+            exec("mkdir " + pth + "js " + pth + "css " + pth + "fonts " + pth + "img");
+            exec("cp " + __dirname + "/node_modules/topcoat/css/topcoat-mobile-light.css " + pth + "css/topcoat-desktop-light.css");
+            exec("cp " + __dirname + "/node_modules/topcoat/demo/css/main.css " + pth + "css/main.css");
+            exec("cp " + __dirname + "/third-party/prism.css " + pth + "css/prism.css");
+            exec("cp " + __dirname + "/third-party/prism.js " + pth + "js/prism.js");
+            exec("cp " + __dirname + "/node_modules/topcoat/demo/fonts/* " + pth + "fonts");
+            exec("cp " + __dirname + "/node_modules/topcoat/img/search.svg " + pth + "img/search.svg");
+            exec("cp " + __dirname + "/node_modules/topcoat/img/search_dark.svg " + pth + "img/search_dark.svg");
+            exec("cp " + __dirname + "/dr.css " + pth + "css/dr.css");
+            // exec("cp " + __dirname + "/dr-print.css " + pth + "dr-print.css");
+        }
+        scripts = json.scripts || [];
+    }
+    console.log("\nTrust me, I am a Dr.js\n");
+    for (i = 0, ii = files.length; i < ii; i++) {
+        var filename = files[i];
+        fileName = fileName || filename;
+        console.log("Processing " + filename);
+        var code = fs.readFileSync(filename, "utf-8"),
+            res = docit(code, filename, srcs[i]);
+        if (res.out) {
+            toc = toc.concat(res.toc);
+            sec = sec.concat(res.out);
+            for (var key in res.chunks) if (res.chunks.hasOwnProperty(key) && !chunks[key]) {
+                chunks[key] = res.chunks[key];
+            }
+            title = title || res.title;
+            console.log("Found \033[32m" + res.sections + "\033[0m section" + ss(res.sections) + ".");
+            console.log("Processing \033[32m" + res.loc + "\033[0m line" + ss(res.loc) + " of code...");
+            srcs[i] || (function (filename) {
+                fs.writeFile(getPath(filename) + "-src.html", res.source, function () {
+                    console.log("Source saved to \033[32m" + getPath(filename) + "-src.html\033[0m\n");
+                });
+            })(filename);
+        } else {
+            console.log("\033[33m⚠  No comments in Dr.js format found\033[0m");
+        }
+    }
+    var TOC = "",
+        RES = "";
+    toc.sort(function (a, b) {
+        if (a.name == b.name) {
+            return 0;
+        }
+        if (a.name < b.name) {
+            return -1;
+        }
+        return 1;
+    });
+
+    var temp = fs.readFileSync(json.template || "template.dot", "utf-8"),
+        betterTOC = [];
+    for (i = 0, ii = toc.length; i < ii; i++) {
+        if (i == ii - 1 || toc[i].name != toc[i + 1].name) {
+            betterTOC.push(toc[i]);
+        }
+    }
+    dot.templateSettings.strip = false;
+    temp = dot.template(temp);
+    var html = temp({
+        out: sec,
+        toc: betterTOC
+    });
+
+    fs.writeFile(output || (getPath(fileName) + ".html"), html, function () {
+        console.log("Saved to \033[32m" + (output || getPath(fileName) + ".html") + "\033[0m\n");
+    });    
 }
 
-if (files.length == 1 && path.extname(ref) == ".json") {
-    var pth = ref.substring(0, ref.length - path.basename(ref).length),
-        json = JSON.parse(fs.readFileSync(files[0], "utf-8"));
-    title = json.title;
-    files = [];
-    for (var i = 0, ii = json.files && json.files.length; i < ii; i++) {
-        files.push(path.resolve(pth, json.files[i].url));
-        srcs.push(json.files[i].link);
-    }
-    output = path.resolve(pth, json.output) || "";
-    pth = output.substring(0, output.length - path.basename(output).length)
-    exec("mkdir " + pth + "js " + pth + "css " + pth + "fonts " + pth + "img");
-    exec("cp " + __dirname + "/node_modules/topcoat/css/topcoat-mobile-light.css " + pth + "css/topcoat-desktop-light.css");
-    exec("cp " + __dirname + "/node_modules/topcoat/demo/css/main.css " + pth + "css/main.css");
-    exec("cp " + __dirname + "/third-party/prism.css " + pth + "css/prism.css");
-    exec("cp " + __dirname + "/third-party/prism.js " + pth + "js/prism.js");
-    exec("cp " + __dirname + "/node_modules/topcoat/demo/fonts/* " + pth + "fonts");
-    exec("cp " + __dirname + "/node_modules/topcoat/img/search.svg " + pth + "img/search.svg");
-    exec("cp " + __dirname + "/node_modules/topcoat/img/search_dark.svg " + pth + "img/search_dark.svg");
-    exec("cp " + __dirname + "/dr.css " + pth + "css/dr.css");
-    // exec("cp " + __dirname + "/dr-print.css " + pth + "dr-print.css");
-    scripts = json.scripts || [];
-}
-console.log("\nTrust me, I am a Dr.js\n");
-for (i = 0, ii = files.length; i < ii; i++) {
-    var filename = files[i];
-    fileName = fileName || filename;
-    console.log("Processing " + filename);
-    var code = fs.readFileSync(filename, "utf-8"),
-        res = docit(code, filename, srcs[i]);
-    if (res.sections && res.source) {
-        toc = toc.concat(res.toc);
-        for (var key in res.chunks) if (res.chunks.hasOwnProperty(key) && !chunks[key]) {
-            chunks[key] = res.chunks[key];
-        }
-        title = title || res.title;
-        console.log("Found \033[32m" + res.sections + "\033[0m section" + ss(res.sections) + ".");
-        console.log("Processing \033[32m" + res.loc + "\033[0m line" + ss(res.loc) + " of code...");
-        srcs[i] || (function (filename) {
-            fs.writeFile(getPath(filename) + "-src.html", res.source, function () {
-                console.log("Source saved to \033[32m" + getPath(filename) + "-src.html\033[0m\n");
-            });
-        })(filename);
-    } else {
-        console.log("\033[33m⚠  No comments in Dr.js format found\033[0m");
-    }
-}
-var TOC = "",
-    RES = "";
-toc.sort(function (a, b) {
-    if (a.name == b.name) {
-        return 0;
-    }
-    if (a.name < b.name) {
-        return -1;
-    }
-    return 1;
-});
-for (i = 0, ii = toc.length; i < ii; i++) if (i == ii - 1 || toc[i].name != toc[i + 1].name) {
-    TOC += format('<li class="dr-lvl{indent}"><a href="#{name}" class="{clas}"><span>{name}{brackets}</span></a></li>', toc[i]);
-    RES += chunks[toc[i].name] || "";
-}
-var html = '<!DOCTYPE html>\n<html>\n  <head>\n    <meta charset="utf-8">\n    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">\n    <title>' + title + ' API Reference</title>\n    <meta name="viewport" content="user-scalable=no,initial-scale = 1.0,maximum-scale = 1.0">\n    <link rel="stylesheet" href="fonts/stylesheet.css"><link rel="stylesheet" href="css/topcoat-desktop-light.css">\n    <link rel="stylesheet" href="css/main.css">\n    <link rel="stylesheet" href="css/dr.css">\n    <link rel="stylesheet" href="css/prism.css"><!--[if lt IE 9]>\n    <script src="//html5shiv.googlecode.com/svn/trunk/html5.js"></script><![endif]-->\n  </head>\n  <body class="light">\n    <div id="wrapper">\n      <div class="max-width">\n        <div id="sideNav">\n          <div class="combo">\n              <input type="search" id="dr-filter" value="" placeholder="search" class="topcoat-search-input">\n          </div>\n          <div id="pageNav"><ol id="dr-toc">' + TOC + '</ol></div>\n        </div>\n      </div>\n      <div id="site">\n        <header id="main-header">\n          <div class="max-width">\n            <hgroup>\n              <h1>' + title + '</h1>\n              <p>API Reference</p>\n            </hgroup>\n          </div>\n        </header>\n        <div id="content" class="max-width">' + RES + '</div>\n        <footer></footer>\n      </div>\n    </div>\n    <script src="//use.edgefonts.net/source-sans-pro:n3,n4,n6;source-code-pro:n3.js"></script>\n    <script src="js/prism.js"></script>\n';
-// var html = '<!DOCTYPE html>\n<!-- Generated with Dr.js -->\n<html lang="en">' +
-//     '<head><meta charset="utf-8"><title>' + title + ' Reference</title>\n' +
-//     '<link rel="stylesheet" href="dr.css" media="screen">\n' +
-//     '<link rel="stylesheet" href="dr-print.css" media="print"></head>\n' +
-//     '<body id="dr-js"><div id="dr"><ol class="dr-toc" id="dr-toc">' + TOC +
-//     '</ol><div class="dr-doc"><h1>' + title + ' Reference</h1>' + RES +
-//     "</div></div>\n";
-for (i = 0, ii = scripts.length; i < ii; i++) {
-    html += '<script src="' + scripts[i] + '"></script>\n';
-}
-html += "<script>" + fs.readFileSync(__dirname + "/toc-min.js", "utf-8") + "</script>\n</body></html>";
-fs.writeFile(output || (getPath(fileName) + ".html"), html, function () {
-    console.log("Saved to \033[32m" + (output || getPath(fileName) + ".html") + "\033[0m\n");
-});
